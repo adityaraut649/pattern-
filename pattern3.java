dbf6229536768af865c5616d2920126edb85a8c0
ADITYA RAUT <95859957+adityaraut649@users.noreply.github.com>
2025-08-21 15:43:03 +0530

    Add files via upload
    ***** 
    **** 
    *** 
    ** 
    *

---
+++ pattern3.java
@@ -0,0 +1,23 @@
+package pattern2;
+   /*   *****
+        ****
+        ***
+        **
+        *   */
+
+
+import java.util.Scanner;
+
+public class pattern3 {
+   public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       int n = scan.nextInt();
+
+       for (int i = 1; i <= n ; i++) {
+           for (int j = 1; j <=n - i + 1 ; j++) {
+               System.out.print("*");
+           }
+           System.out.println(" ");
+       }
+    }
+}
